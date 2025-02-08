1b36a4c340de516994993423f639f09379a40483
bencelder <benc.elder@gmail.com>
2025-02-07 14:53:59 -1000

    template for clock constraints on mu

---
+++ mu_constraints.py
@@ -0,0 +1,13 @@
+"""
+mu_constraints.py
+
+"""
+
+def DE_max_amplitude(clock_pair = "CaF"):
+    return 1
+
+def DM_max_amplitude(clock_pair = "CaF"):
+    return 1
+
+def MG_max_amplitude(w = 1, clock_pair = "Caf"):
+    return 1
